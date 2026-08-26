43e47e124ae1680ac743c5391bb428a3961b0a8e
Gurutz Ocon <gurutz.ocon@gmail.com>
2026-08-26 08:57:53 +0200

    Treasury ready to receive and withdraw funds

--- src/DaoTreasury.sol
+++ src/DaoTreasury.sol
@@ -28,4 +28,41 @@ contract DaoTreasury is Ownable {
         emit DaoSet(_daoAddr);
     }
 
+    function fundTreasuryWithEth() external payable {
+        require(msg.value > 0, "Must fund ETH");
+        emit TreasuryFunded(msg.sender, msg.value);
+    }
+
+    function fundTreasuryWithToken(address token, uint256 amount) external {
+        require(token != address(0), "Invalid token address");
+        require(amount > 0, "Amount must be greater than zero");
+
+        IERC20 tokenContract = IERC20(token);
+        bool success = tokenContract.transferFrom(msg.sender, address(this), amount);
+        require(success, "Token transfer failed");
+
+        emit TreasuryFunded(msg.sender, amount);
+    }
+
+    // This function allows the contract to receive ETH
+    receive() external payable {
+        emit TreasuryFunded(msg.sender, msg.value);
+    }
+
+    function emergencyWithdraw(address token, uint256 amount, address recipient) external onlyOwner {
+        require(recipient != address(0), "Invalid recipient address");
+        require(amount > 0, "Amount must be greater than zero");
+
+        if (token == address(0)) {  // Withdraw ETH
+            require(address(this).balance > 0, "Insufficient ETH balance");
+            (bool success,) = recipient.call{value: amount}("");
+            require(success, "ETH withdrawal failed");
+
+        } else {
+            IERC20 tokenContract = IERC20(token);
+            require(tokenContract.balanceOf(address(this)) > 0, "Insufficient token balance");
+            bool success = tokenContract.transfer(recipient, amount);
+            require(success, "Token withdrawal failed");
+        }
+    }
 }
